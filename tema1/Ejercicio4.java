d449f45371b35ca8b06f784bc1040bc0ce2cdff0
azro871 <zrouquiaya1@gmail.com>
2026-09-24 09:09:16 +0200

    Add files via upload

---
+++ tema1/Ejercicio4.java
@@ -0,0 +1,16 @@
+public class Ejercicio4 {
+    public static void main(String[] args) {
+
+        System.out.println("English\tEspañol");
+        System.out.println("Water\tAgua");
+        System.out.println("Car\tCoche");
+        System.out.println("Beach\tPlaya");
+        System.out.println("Blue\tAzul");
+        System.out.println("Food\tComida");
+        System.out.println("Summer\tVerano");
+        System.out.println("Friend\tAmigo");
+        System.out.println("Family\tFamilia");
+        System.out.println("Table\tTabla");
+        System.out.println("Window\tVentana");
+    }
+}
