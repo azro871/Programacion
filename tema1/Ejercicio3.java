d9365c55313a7f5a32d3cf5a58ce4b557fd8a748
azro871 <zrouquiaya1@gmail.com>
2026-09-24 08:44:30 +0200

    Add files via upload

---
+++ tema1/Ejercicio3.java
@@ -0,0 +1,7 @@
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        System.out.println("Aya");
+        System.out.println("Moguer,Calle Ribera ");
+        System.out.println("642098534");
+    }
+}
