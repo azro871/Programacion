0546a523249492666581ee59d48ef824268dbaeb
azro871 <zrouquiaya1@gmail.com>
2026-09-25 12:19:01 +0200

    Add files via upload

---
+++ tema1/Ejercicio5.java
@@ -0,0 +1,17 @@
+public class Horario {
+    public static void main(String[] args) {
+        System.out.println("  Horas&Días    Lunes     Martes    Miercoles   Jueves   Viernes  ");
+        System.out.println("------------------------------------------------------------------");
+        System.out.println("  1ªHora        LMDAW     BDDAW     SIDAW       PRDAW    EDDAW    ");
+        System.out.println("------------------------------------------------------------------");
+        System.out.println("  2ªHora        LMDAW     BDDAW     SIDAW       PRDAW    BDDAW    ");
+        System.out.println("------------------------------------------------------------------");
+        System.out.println("  3ªHora        SIDAW     SOS       ITEC        LMDAW    BDDAW    ");
+        System.out.println("------------------------------------------------------------------");
+        System.out.println("  4ªHora        SIDAW     EDDAW     ITEC        SIDAW    ITEC     ");
+        System.out.println("------------------------------------------------------------------");
+        System.out.println("  5ªHora        PRDAW     PRDAW     DIG         BDDAW    PRDAW    ");
+        System.out.println("------------------------------------------------------------------");
+        System.out.println("  6ªHora        PRDAW     PRDAW     EDDAW       BDDAW    PRDAW    ");
+    }
+}
